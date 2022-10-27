f3dc52f86f3db380adf83681e1eaf7672c35a1eb
Pablo <94941513+pablo-de@users.noreply.github.com>
2022-10-27 12:31:44 -0300

    Update tf-modules.sh

--- tf-modules.sh
+++ tf-modules.sh
@@ -1,75 +1,93 @@
 #!/bin/bash
 
-#This script download the latest version of terraform modules from github and push to gitlab repository.
+#Script to download the latest version of terraform modules from github.
 
 #Variables.
-TF_DIR_TPM="/home/$USER/Practicas/bash/tf/github-repo"
-TF_MODULES_DIR="/home/$USER/Practicas/bash/tf/modules"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+NOW=$(date +"%d-%m-%Y - %H:%M")
 
-TF_S3_REPO="https://github.com/terraform-aws-modules/terraform-aws-s3-bucket"
-TF_APIGATEWAY_REPO="https://github.com/terraform-aws-modules/terraform-aws-apigateway-v2"
-TF_ECR_REPO="https://github.com/terraform-aws-modules/terraform-aws-ecr"
-TF_SECURITYGROUP_REPO="https://github.com/terraform-aws-modules/terraform-aws-security-group"
-TF_EC2_REPO="https://github.com/terraform-aws-modules/terraform-aws-ec2-instance"
-TF_EKS_REPO="https://github.com/terraform-aws-modules/terraform-aws-eks"
-TF_RDS_REPO="https://github.com/terraform-aws-modules/terraform-aws-rds"
-TF_VPC_REPO="https://github.com/terraform-aws-modules/terraform-aws-vpc"
+MODULES_DIR="$DIR/modules"
+MODULES_GITLAB="$DIR/gitlab"
 
-#Check if the directories exists if not create it.
-if [ ! -d "$TF_MODULES_DIR" ]; then
-    mkdir -p "$TF_MODULES_DIR"
-fi
+#Array with the modules.
+declare -a TF_MODULES=(
+    tf-aws-s3="https://github.com/terraform-aws-modules/terraform-aws-s3-bucket"
+    tf-aws-apigateway="https://github.com/terraform-aws-modules/terraform-aws-apigateway-v2"
+    # tf-aws-ecr="https://github.com/terraform-aws-modules/terraform-aws-ecr"
+    # tf-aws-security-group="https://github.com/terraform-aws-modules/terraform-aws-security-group"
+    # tf-aws-eks="https://github.com/terraform-aws-modules/terraform-aws-eks"
+    # tf-aws-rds="https://github.com/terraform-aws-modules/terraform-aws-rds"
+    # tf-aws-vpc="https://github.com/terraform-aws-modules/terraform-aws-vpc"
+    # tf-aws-ec2="https://github.com/terraform-aws-modules/terraform-aws-ec2-instance"
+    # tf-aws-autoscaling="https://github.com/terraform-aws-modules/terraform-aws-autoscaling"
+    # tf-aws-elb="https://github.com/terraform-aws-modules/terraform-aws-elb"
+)
 
-if [ ! -d "$TF_DIR_TPM" ]; then
-    mkdir -p "$TF_DIR_TPM"
-fi
+#Function to download the modules.
+function tf_download_dir {
 
-#Function to check if is the directory exist and check if the last version of the module is downloaded. If not, download and copy the new version.
-function tf_check_dir {
-    if [ -d $1 ]; then
-        cd $1
-        git pull
-        #If git pull copy the new modules to the modules directory.
-        if [ $? -eq 0 ]; then
-            cp -r $TF_DIR_TPM/$1 $TF_MODULES_DIR
-            #Move to the new directory.
-            cd $TF_MODULES_DIR/$1
-            #Remove files except .tf files.
-            find . -type f ! -name '*.tf' -delete
-            #Remove directories.
-            find . -type d ! -name '.' -exec rm -rf {} +
-            cd $TF_DIR_TPM
-        fi
+    #Check if the directory "$MODULES_DIR"/$1 exists or create it.
+    if [ ! -d "$MODULES_DIR"/$1 ]; then
+        mkdir -p "$MODULES_DIR"/$1
+    fi
+
+    cd "$MODULES_DIR"/$1 || exit
+
+    #Check if remote upstream already exists and is already up to date.
+    if [ ! -z "$(git remote -v | grep upstream)" ] && [ ! -z "$(git pull upstream master)" ]; then
+        #Print no changes with date.
+        echo "$NOW | No changes in $1 module" >>"$DIR"/changes.log
     else
-        git clone $2
+        #Check git init.
+        if [ ! -d "$MODULES_DIR"/$1/.git ]; then
+            git init
+        fi
 
-        #Copy the directories to the modules directory.
-        cp -r $TF_DIR_TPM/$1 $TF_MODULES_DIR
-        #Move to the new directory.
-        cd $TF_MODULES_DIR/$1
-        #Remove files except .tf files.
-        find . -type f ! -name '*.tf' -delete
-        #Remove directories.
-        find . -type d ! -name '.' -exec rm -rf {} +
+        git remote add upstream "$2"
+        git pull upstream master
+        git pull upstream master --allow-unrelated-histories
+        git fetch --tags upstream
 
-        #Push to gitlab repository.
-        # cd $TF_MODULES_DIR
-        # git add .
-        # git commit -m "Add $1 module"
-        # git push
+        #Get the latest tag and save into a variable tag.
+        tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 
-        cd $TF_DIR_TPM
+        #Write in TAG.md the last tag.
+        echo "Version: $tag" >README.md
+
+        # Check if the directory gitlab exists.
+        if [ ! -d "$MODULES_GITLAB/$1" ]; then
+            mkdir -p "$MODULES_GITLAB/$1"
+        fi
+
+        # Copy .tf and tag.md files to gitlab directory.
+        cp -r "$MODULES_DIR"/$1/*.tf "$MODULES_GITLAB"/$1
+        cp -r "$MODULES_DIR"/$1/README.md "$MODULES_GITLAB"/$1
+
+        #Print changes with date in format in log file.
+        echo "$NOW | New module $1 = $tag" >>"$DIR"/changes.log
+
+        # CD "$MODULES_GITLAB"
+
+        # #Check git init.
+        # if [ ! -d "$MODULES_GITLAB"/.git ]; then
+        #     git init
+        # fi
+
+        # git add "$MODULES_GITLAB"/$1
+        # git commit -m "Add module $1 = $tag"
+        # git push origin master
+
+        cd "$MODULES_DIR" || exit
+
     fi
 
 }
 
-#Call the function for each module.
-cd $TF_DIR_TPM
-tf_check_dir "terraform-aws-s3-bucket" $TF_S3_REPO
-tf_check_dir "terraform-aws-apigateway-v2" $TF_APIGATEWAY_REPO
-tf_check_dir "terraform-aws-ecr" $TF_ECR_REPO
-tf_check_dir "terraform-aws-security-group" $TF_SECURITYGROUP_REPO
-tf_check_dir "terraform-aws-ec2-instance" $TF_EC2_REPO
-tf_check_dir "terraform-aws-eks" $TF_EKS_REPO
-tf_check_dir "terraform-aws-rds" $TF_RDS_REPO
-tf_check_dir "terraform-aws-vpc" $TF_VPC_REPO
+#Download the modules.
+for i in "${TF_MODULES[@]}"; do
+    #Split the array.
+    IFS='=' read -ra ADDR <<<"$i"
+    #Call the function.
+    tf_download_dir ${ADDR[0]} ${ADDR[1]}
+done
+
